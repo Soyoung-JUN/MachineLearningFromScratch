004f9fd6fa0514b483f36eeae8f3a27ea154bf1b
Soyoung-JUN <58747898+Soyoung-JUN@users.noreply.github.com>
2020-07-17 21:08:47 +0900

    Update and rename main.py to week1_hw1_jsy.py

--- Homework/Homework#1/week1_hw1_jsy.py
+++ Homework/Homework#1/week1_hw1_jsy.py
@@ -1,10 +1,10 @@
 ####################################################################################
-# File Name :                                                                      #  
-# Date  : 2020/00/00                                                               #  
+# File Name : week1_hw1_jsy                                                                     #  
+# Date  : 2020/07/17                                                               #  
 # OS : Windows 10                                                                  #  
-# Author :                                                                         #
+# Author : Soyoung JUN                                                                        #
 # -------------------------------------------------------------------------------  #  
-# requirements : python 3.x                                                        #
+# requirements : python 3.7                                                        #
 #                                                                                  #
 ####################################################################################   
 
@@ -13,6 +13,7 @@ import numpy as np
 import pandas as pd                 
 import matplotlib.pyplot as plt    
 import warnings                     
+
 
 try:
     from sklearn.cluster import KMeans  # check installation of sklearn
@@ -23,4 +24,10 @@ except:
 
 if __name__ == '__main__': # Start from main
     #Implement Load and Plot Code here
+    df = pd.read_csv("data.csv")
+    # Basic Scatter Plot
+    plt.plot('Sepal length','Sepal width', data=df, linestyle='none', marker='o', markersize=10, color='blue', alpha=0.5)
+    plt.title('from scikit-learn library', fontsize=20)
+    plt.xlabel('sepal length(cm)', fontsize=14)
+    plt.ylabel('sepal width(cm)', fontsize=14)    
     plt.show() # show plot
